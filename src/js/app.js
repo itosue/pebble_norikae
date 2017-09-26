367133a87ef5f6ba62e926dfec5ac2c20ba5417d
itosue <itosue@users.noreply.github.com>
2017-09-27 00:01:34 +0900

    simply.jsからPebble.jsにアーキテクチャ変更
    命令セットをsimply.jsからPebble.jsに変更（新たにPTRに対応しました）
    電車の種類で色を変えるロジック追加
    まさに今出発する電車が表示されても仕方ないので現在時刻に１分足して直近の電車を検索するように変更

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+var UI = require('ui');
+
 //発駅と着駅を設定してURLエンコード
 //駅名はjorudanの乗換案内で検索した時に補完候補で出てくる名前で入力してください
 var strFromSta="高槻";	//発駅
@@ -7,6 +9,7 @@ var utf8strToSta = encodeURIComponent(strToSta);
 
 //URLに埋め込む日付をパース
 var today = new Date();
+today.setMinutes(today.getMinutes() + 1);
 var y=''+today.getFullYear();
 var m=today.getMonth()+1;
 m=m<10?'0'+m:''+m;
@@ -17,15 +20,31 @@ mm=mm<10?'0'+mm:''+mm;
 var m1=mm[0];
 var m2=mm[1];
 
+var datetime1 = '';
+var datetime2 = '';
+
+var ajax = require('ajax');
+
+var col1 = 'Blue';
+var col2 = 'Blue';
+
 //URLを作成して実行→結果からデータ取り出し
 var url = 'http://www.jorudan.co.jp/norikae/cgi/nori.cgi?eki1='+utf8strFromSta+'&eki2='+utf8strToSta+'&eki3=&via_on=1&Dym='+y+m+'&Ddd='+d+'&Dhh='+hh+'&Dmn1='+m1+'&Dmn2='+m2+'&Cway=0&Cfp=1&C7=1&C2=0&C3=0&C1=0&C4=0&C6=2&S.x=44&S.y=9&S=%E6%A4%9C%E7%B4%A2&Cmap1=&rf=nr&pg=0&eok1=&eok2=&eok3=&Csg=1';
+// console.log(url);
 ajax({ url: url }, function(data){
+	//console.log('test1'+data);
+	
+	// 電車の種類で色を変える時はここで指定
+	if ( data.match(/新快速/) || data.match(/みずほ/)){
+		col1='Red';
+	}
+	
 	var j = data.match(/<div class="_time">(.*?)<\/div>/)[1];
 	j=j.replace(/\d+\/\d+/g,'');
 	j=j.replace(/\D/ig,'');
-	simply.title(''); //タイムアウトした時にsimply.jsのデフォルト文字列が出るのを上書き
-	simply.subtitle('\n'+j[0]+j[1]+':'+j[2]+j[3]+'-'+j[4]+j[5]+':'+j[6]+j[7]);
-
+	datetime1='\n'+j[0]+j[1]+':'+j[2]+j[3]+'-'+j[4]+j[5]+':'+j[6]+j[7];
+	
+	
 	//以下同様に次の列車を検索
 	var ndate=new Date(y,m-1,d,j[0]+j[1],j[2]+j[3]);
 	ndate.setMinutes(ndate.getMinutes()+1);
@@ -42,10 +61,35 @@ ajax({ url: url }, function(data){
 
 	var nurl = 'http://www.jorudan.co.jp/norikae/cgi/nori.cgi?eki1='+utf8strFromSta+'&eki2='+utf8strToSta+'&eki3=&via_on=1&Dym='+ny+nm+'&Ddd='+nd+'&Dhh='+nhh+'&Dmn1='+nm1+'&Dmn2='+nm2+'&Cway=0&Cfp=1&C7=1&C2=0&C3=0&C1=0&C4=0&C6=2&S.x=44&S.y=9&S=%E6%A4%9C%E7%B4%A2&Cmap1=&rf=nr&pg=0&eok1=&eok2=&eok3=&Csg=1';
 	ajax({ url: nurl }, function(data){ 
+		// 電車の種類で色を変える時はここで指定
+		if ( data.match(/新快速/) || data.match(/みずほ/)) {
+			col2='Red';
+		}
 		var j = data.match(/<div class="_time">(.*?)<\/div>/)[1];
 		j=j.replace(/\d+\/\d+/g,'');
 		j=j.replace(/\D/ig,'');
 
-		simply.body('\nnext\n'+j[0]+j[1]+':'+j[2]+j[3]+'-'+j[4]+j[5]+':'+j[6]+j[7]);
+		datetime2='\nnext\n'+j[0]+j[1]+':'+j[2]+j[3]+'-'+j[4]+j[5]+':'+j[6]+j[7];
+	
+	
+		var main = new UI.Card({
+			title: datetime1,
+			subtitle: '',
+			body: datetime2,
+			titleColor: col1,
+			subtitleColor: 'indigo', // Named colors
+			bodyColor: col2 // Hex colors
+		});
+
+		main.show();	
+	
+	
 	});
+	
+
+	
+	
 });
+
+
+
